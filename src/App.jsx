9a63f161e69e1056ab8ec479e00cbee384975bd3
GarlicBuns19 <planterx26@gmail.com>
2023-04-27 11:44:15 +0200

    feat: Choose a Joke Type

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,12 @@ function App() {
     const [loading, setLoading] = useState(false);
     const [joke, setJoke] = useState({});
     const [display, setDisplay] = useState(false);
+    const [option, setOption] = useState('Programming');
     const getJoke = async () => {
 
         setLoading(true)
 
-        await fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist&type=twopart")
+        await fetch(`https://v2.jokeapi.dev/joke/${option}?blacklistFlags=nsfw,religious,political,racist,sexist&type=twopart`)
             .then(res => res.json())
             .then(data => setJoke(data))
             .catch(e => {
@@ -30,29 +31,44 @@ function App() {
         setDisplay(true)
     }
 
+    const changeJokeType = () => {
+        setOption(document.getElementById('jokeType').value);
+    }
+
     useEffect(() => {
         getJoke();
     }, []);
 
     return (
         <>
-            {loading === true ? <div>Loading Joke</div> :
-                <div className={'wrapper'}>
-                    {joke.error === true ? <><h2>Joke could not be displayed. Please try again.</h2><div><Button onClick={getJoke}>Get Joke!</Button></div></>
+            <div className={'wrapper'}>
+                <label htmlFor="jokeType">Choose Joke Type</label><br></br>
+
+                <select name="jokeType" id="jokeType" onChange={changeJokeType}>
+                    <option value="Programming">Programming</option>
+                    <option value="Dark">Dark</option>
+                    <option value="Miscellaneous">Miscellaneous</option>
+                    <option value="Christmas">Christmas</option>
+                </select>
+
+                {loading === true ? <div>Loading Joke</div> :
+                    joke.error === true ? <>
+                            <h2>Joke could not be displayed. Please try again.</h2>
+                            <div><Button onClick={getJoke}>Get Joke!</Button></div>
+                        </>
                         : <>
                             {Object.keys(joke).length > 0 ?
                                 <>
                                     <h2>{joke.setup}</h2>
-                                    {display === true ? <div><h3>{joke.delivery}</h3></div> :
-                                        <Button onClick={showDeliveryOfJoke}>Show the joke!</Button>}
+                                    {display === true ? <div><h3>{joke.delivery}</h3></div> : <Button onClick={showDeliveryOfJoke}>Show the joke!</Button>}
                                 </>
                                 : <h2>Joke could not be displayed. Please try again.</h2>}
                             <div>
                                 <Button onClick={getJoke}>Get Joke!</Button>
                             </div>
-                        </>}
-                </div>
-            }
+                        </>
+                }
+            </div>
         </>
     )
 }
