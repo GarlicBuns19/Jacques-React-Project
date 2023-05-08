57b12e28d359bad6e8bc78abacd423e49ad16821
GarlicBuns19 <planterx26@gmail.com>
2023-05-08 17:47:36 +0200

    Added styling using Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -37,10 +37,11 @@ function App() {
     }, [jokeOption]);
 
     return (
-        <div className={"container p-4"}>
+        <div className={"container bg-slate-900 min-h-screen text-white mx-auto p-4"}>
             <InputWrapper>
-                <label htmlFor="jokeType">Choose Joke Type</label><br></br>
-                <select name="jokeType" id="jokeType" value={jokeOption} onChange={(e) => setJokeOption(e.target.value)}>
+                <label htmlFor="jokeType">Choose Joke Type</label>
+                <select name="jokeType" id="jokeType" className={"bg-slate-900"} value={jokeOption}
+                        onChange={(e) => setJokeOption(e.target.value)}>
                     <option value="Programming">Programming</option>
                     <option value="Dark">Dark</option>
                     <option value="Miscellaneous">Miscellaneous</option>
@@ -50,24 +51,24 @@ function App() {
 
             {loading ? <div>Loading Joke</div> :
                 joke.error ?
-                    <>
+                    <div>
                         <h2>Joke could not be displayed. Please try again.</h2>
                         <div><Button onClick={getJoke}>Try to get a joke again!</Button></div>
-                    </>
-                    : <>
+                    </div>
+                    : <div>
                         {Object.keys(joke).length > 0 ?
                             <>
-                                <h2>{joke.setup}</h2>
+                                <h2 className={"text-lg"}>{joke.setup}</h2>
                                 {jokeDisplay ?
-                                    <div><h3>{joke.delivery}</h3></div> :
-                                    <Button onClick={() => setJokeDisplay(true)}>Show the joke!</Button>}
+                                    <div><h3 className={"text-xl text-sky-400"}>{joke.delivery}</h3></div> :
+                                    <Button className={"hover:bg-slate-800 ease-in duration-300"} onClick={() => setJokeDisplay(true)}>Show the joke!</Button>}
                             </>
                             :
                             <h2>Joke could not be displayed. Please try again.</h2>}
                         <div>
-                            <Button onClick={getJoke}>Get Joke!</Button>
+                            <Button className={"hover:bg-slate-800 ease-in duration-300"} onClick={getJoke}>Get Joke!</Button>
                         </div>
-                    </>
+                    </div>
             }
         </div>
     )
